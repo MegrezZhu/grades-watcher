4b74f099389ddc977d0689f23a537446b0604224
Zhu Chu <mystery490815101@gmail.com>
2017-07-14 00:36:37 +0800

    add global error handling

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ const watcher = new Watcher(config);
 watcher.start({
   minute: _.range(0, 60, 5)
 });
+
+process.on('uncaughtException', err => {
+  watcher.send('错误', err.stack);
+});
+
+process.on('unhandledRejection', err => {
+  watcher.send('错误', err.stack);
+});
